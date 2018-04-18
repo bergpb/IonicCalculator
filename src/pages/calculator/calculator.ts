1d6c9f4a7198cb3fd54e0ffac28f2c34bf4ec1a4
bergpb <lindembergdepaulo@gmail.com>
2018-04-18 00:06:58 -0300

    conversoes para binario, octal e hexadecimal adicionados

--- src/pages/calculator/calculator.ts
+++ src/pages/calculator/calculator.ts
@@ -5,35 +5,46 @@ import { NavController } from 'ionic-angular';
     selector: 'page-calculator',
     templateUrl: 'calculator.html'
 })
+
 export class CalculatorPage {
+
     resultnormal = '';
-    resultbin = '';
-    resultoct = '';
-    resulthex = '';
+    resultbin = '0';
+    resultoct = '0';
+    resulthex = '0';
 
     constructor(public navCtrl: NavController) {}
+    
+    convertBin(resultnormal, base){
+        this.resultbin = parseInt(resultnormal, 10).toString(2);
+    }
 
-    //https://gist.github.com/faisalman/4213592
-    //https://angular.io/api/forms/NgModel
+    convertOct(resultnormal, base){
+        this.resultoct = parseInt(resultnormal, 10).toString(8);
+    }
+
+    convertHex(resultnormal, base){
+        this.resulthex = parseInt(resultnormal, 10).toString(16);
+    }
 
     btnClicked(btn) {
         if (btn == 'C') {
             this.resultnormal = '';
-            this.resultbin = '';
-            this.resultoct = '';
-            this.resulthex = '';
+            this.resultbin = '0';
+            this.resultoct = '0';
+            this.resulthex = '0';
         }
         else if (btn == '=') {
             this.resultnormal = eval(this.resultnormal);
-            this.resultbin = 'teste';
-            this.resultoct = 'teste';
-            this.resulthex = 'teste';
+            this.convertBin(this.resultnormal, 2);
+            this.convertOct(this.resultnormal, 8);
+            this.convertHex(this.resultnormal, 16);
          }
         else {
             this.resultnormal += btn;
-            this.resultbin = '';
-            this.resultoct = '';
-            this.resulthex = '';
+            this.convertBin(this.resultnormal, 2);
+            this.convertOct(this.resultnormal, 8);
+            this.convertHex(this.resultnormal, 16);
         }
     }
 }
